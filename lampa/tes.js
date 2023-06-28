8263114c56dc618848134744e346ffddec225cc2
fiduchi <fiduchi@protonmail.com>
2023-06-28 21:01:23 +0300

    test7

--- lampa/tes.js
+++ lampa/tes.js
@@ -1,6 +1,14 @@
 (function () {
     'use strict';
 
-Console.log('Ваше сообщение');
+    function startSnow() {
+      window.snow = true;
+      Lampa.Listener.follow('app', function (e) {
+        if (e.type == 'ready') {
+          $('.background').snowfall({deviceorientation : true, round : true, minSize: 5, maxSize:8});
+        }
+      });
+    }
+    if (!window.snow) startSnow();
 
 })();
